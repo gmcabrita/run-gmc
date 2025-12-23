2f8649e9e0a667cfd5dc9d3d33d6977264b06d26
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2025-12-23 23:04:33 +0000

    Global coverflex module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { addCoverflexEndpoints } from "./coverflex";
+import { addCoverflexEndpoints } from "@coverflex";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 addCoverflexEndpoints(app);
