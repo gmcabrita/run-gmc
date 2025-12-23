4c03aed438bcbc8af1fd3ebba4497d182acfae98
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2025-12-23 21:18:52 +0000

    Test CI deploy

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Cloudflare Workers!");
+  return c.text("Hello again, Cloudflare Workers!");
 });
 
 export default app;
